68fb41e4f72464d137a9ddedffc4229a844a03e8
amit10shaw <82945738+amit10shaw@users.noreply.github.com>
2023-09-16 14:18:44 +0530

    Add files via upload

---
+++ task2_positivenumbers.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 16 13:59:49 2023
+
+@author: 69ami
+"""
+
+mylist = [12, -7, 5, 64, -14]
+print(mylist[0]) 
+print(mylist[2])
+print(mylist[3])
+
+
+mylist = [12, 14, -95, 3]
+print(mylist[0]) 
+print(mylist[1]) 
+print(mylist[3]) 
+
+
